d1678472b2764fbfc5485d322de016f1ba611235
NABrainn <michal2002.60@gmail.com>
2023-11-07 11:42:57 +0100

    fix several issues to prevent users from inserting multiple operators in a row and at the start of queation, inserting values is no longer possible after pressing '=' button

--- calculator-project.js
+++ calculator-project.js
@@ -1,25 +1,65 @@
-const firstNum = 5;
-const secondNum = 7;
-const operator = "+";
+let firstNum = '';
+let secondNum = '';
+let operator = '';
 
-function add(operator, firstNum, secondNum) {
-    return firstNum + operator + secondNum;
+function add(firstNum, secondNum) {
+    return Number(firstNum) + Number(secondNum);
 }
 
-function subtract() {
+function subtract(firstNum, secondNum) {
+    return Number(firstNum) - Number(secondNum);
+}
+
+function multiply(firstNum, secondNum) {
+    return Number(firstNum) * Number(secondNum);
+}
+
+function divide(firstNum, secondNum) {
+    return Number(firstNum) / Number(secondNum);
+}
+
+function operate(firstNum) {
+    operationResult.innerText += '=';
+    operationResult.innerText += firstNum;
     
 }
 
-function multiply() {
-    
+const operationResult = document.getElementById('operation-result');
+
+const firstNumListener = function() {
+    firstNum += this.textContent;
+    operationResult.innerText += this.textContent;
 }
 
-function divide() {
-    
+const secondNumListener = function() {
+    secondNum += this.textContent;
+    operationResult.innerText += this.textContent;
+    if (operator === '+') firstNum = add(firstNum, secondNum);
+    else if (operator === '-') firstNum = subtract(firstNum, secondNum);
+    else if (operator === '*') firstNum = multiply(firstNum, secondNum);
+    else if (operator === '/') firstNum = divide(firstNum, secondNum);  
 }
 
-function operate(operator, firstNum, secondNum) {
-    return add(operator, firstNum, secondNum);
-}
+document.querySelectorAll('.digits').forEach(digit => {
+    digit.addEventListener('click', firstNumListener);
+})
 
-console.log(operate(firstNum, secondNum, operator));
+document.querySelectorAll('.operators').forEach(item => {
+    item.addEventListener('click', () => {
+        operator = item.innerText;
+        operationResult.innerText += item.innerText;
+        document.querySelectorAll('.digits').forEach(digit => {
+            digit.removeEventListener('click', firstNumListener);
+            digit.addEventListener('click', secondNumListener);
+        }) 
+        secondNum = '';
+    })
+})
+      
+const operatorEqual = document.getElementById('operator-equal');
+operatorEqual.addEventListener('click', () => {
+    operate(firstNum, secondNum, operator);
+});
+
+
+
